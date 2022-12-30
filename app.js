80e09e736857026ff0964ed5ae10732f403c9d2e
BONGJIN CHO <bjchomr@naver.com>
2022-12-30 19:15:17 +0900

    npm세팅

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const app = express();
 app.use(express.json())
 const port = 6000;
 
+app.get('/querystring', (req, res) => {
+    res.send('querystring 테스트 페이지 입니다')
+  ))
+
+app.post('/body-formdata', (req, res) => {
+    res.send('body-formdata 테스트 페이지 입니다')
+  ))
+
+app.post('/body-multipart', (req, res) => {
+    res.send('body-multipart 테스트 페이지 입니다')
+  ))
+
+app.post('/body-json', (req, res) => {
+    res.send('body-json 테스트 페이지 입니다')
+  ))
+
 app.listen(port, () => {
     console.log(port, '포트로 서버가 열렸어요')
 });
