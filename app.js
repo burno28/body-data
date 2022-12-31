5e3776fd6a1c6cc7aebc0e7e11fcaa5dea7ef44f
BONGJIN CHO <bjchomr@naver.com>
2022-12-31 19:59:56 +0900

    늦은 숙제

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-//노드 body data 기능 실습
+//노드 body data 기능 실습 포트 6000주의
 const express = require('express');
 
 const app = express();
-
-app.use(express.json())
 const port = 6000;
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 app.get('/querystring', (req, res) => {
-    res.send('querystring 테스트 페이지 입니다')
+    const id = req.query.id
+    res.send('id')
+    // console.log(req.query.id)
+    // console.log()
+    // res.send('querystring 테스트 페이지 입니다')
+})
+
+app.get("/", (req, res) => {
+    res.send('<form action="/" enctype="multiaprt/form-data" method="post"><input name="id"/><button  type="submit">전송</button></form>')
+})
+
+app.post("/", (req, res) => {
+    const id = req.body.id
+    res.send(id)
 })
 
 app.post('/body-formdata', (req, res) => {
